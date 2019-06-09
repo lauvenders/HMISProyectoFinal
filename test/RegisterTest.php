b894faa998647a409b437e442c1251996dca269a
Laura Aguilera Checa <lac743@inlumine.ual.es>
2019-06-09 14:00:20 +0200

    registerTest

--- test/RegisterTest.php
+++ test/RegisterTest.php
@@ -14,22 +14,30 @@ final class RegisterTest extend TestCase
 
     public function emptyEmailTest() : void
     {
-
+        $this -> funciones = new functions();
+        $this -> funciones -> set('user', '', 'password', 'password');
+        $this -> assertEquals(array_pop($this -> funciones -> get()), "Email is required");
     }
 
     public function emptyPasswordTest() : void
     {
-
+        $this -> funciones = new functions();
+        $this -> funciones -> set('user', 'email', '', 'password');
+        $this -> assertEquals(array_pop($this -> funciones -> get()), "Password is required");
     }
 
     public function passwordNotMatchTest() : void
     {
-
+        $this -> funciones = new functions();
+        $this -> funciones -> set('user', 'email', 'password', 'password2');
+        $this -> assertEquals(array_pop($this -> funciones -> get()), "The two passwords do not match");
     }
 
     public function correctRegistrationTest() : void 
     {
-
+        $this -> funciones = new functions();
+        $this -> funciones -> set('user', 'email', 'password', 'password');
+        $this -> assertTrue(empty($this -> funciones -> get()));
     }
 }
 ?>
