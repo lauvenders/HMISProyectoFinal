72074985b1618cd439cdd0936d1c379acccd2d35
Adalid Villanueva <avh465@inlumine.ual.es>
2019-06-09 02:58:26 +0200

    prueba

--- admin/list_users.php
+++ admin/list_users.php
@@ -36,18 +36,7 @@
           </tr>
       </thead>
       <tbody>
-          <?php
-          $query = mysql_query("SELECT * FROM multi_login where user_type = "user"");
-          while($row = mysql_fetch_array($query)){
-              echo "<tr>";
-              echo "<td>".$row['id']."</td>";
-              echo "<td>".$row['username']."</td>";
-              echo "<td>".$row['email']."</td>";
-              echo "<td>".$row['user_type']."</td>";
-              echo "<td>".$row['password']."</td>";
-              echo "</tr>";
-          }
-          ?>
+
       </tbody>
     </table>
   </body>
