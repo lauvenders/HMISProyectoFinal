b1caaab18e1c63ce09b55a4fd78018d6525b6efc
Adalid Villanueva <avh465@inlumine.ual.es>
2019-06-09 17:08:17 +0200

    test

--- admin/list_users.php
+++ admin/list_users.php
@@ -39,7 +39,7 @@
                 .$row['email']."</td><td>"
                 .$row['user_type']."</td><td>"
                 .$row['password']."</td><td>
-                <div><a href= 'edit_user.php?edit=<?php echo $row['id']; ?>' class= 'edit_btn' >Edit</a></div>
+                <div><a href=edit_user.php?edit=<?php echo $row['id'];?>class=edit_btn>Edit</a></div>
                 <div><a href=delete_user.php>Delete</a></div>
                 </td></tr>";
             }
