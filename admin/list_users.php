c1fc98d9ebd41229ee16243aed9913c43d0fc9bc
Adalid Villanueva <avh465@inlumine.ual.es>
2019-06-09 13:26:41 +0200

    prueba

--- admin/list_users.php
+++ admin/list_users.php
@@ -38,7 +38,7 @@
                 .$row['username']."</td><td>"
                 .$row['email']."</td><td>"
                 .$row['user_type']."</td><td>"
-                .$row['password']."</td><td><div>Edit User</div><div>Delete User</div></td></tr>";
+                .$row['password']."</td><td><div><a href=""edit.php"">+Edit User</a></div><div>Delete User</div></td></tr>";
             }
           ?>
         </tbody>
