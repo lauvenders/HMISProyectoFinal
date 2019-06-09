f138972c14544d9e9c322c02243c7deac5a9107f
Adalid Villanueva <avh465@inlumine.ual.es>
2019-06-09 04:05:16 +0200

    prueba

--- admin/list_users.php
+++ admin/list_users.php
@@ -36,7 +36,7 @@
         if($conn -> connect_error){
           die("Connection failed: ". $conn-> connect_error);
         }
-        $sql = "SELECT * FROM `users` WHERE 1";
+        $sql = "SELECT * FROM users";
         $result = $conn->$query($sql);
         if($result -> num_rows > 0){
           while($row = $result->fetch_assoc()){
