0eee678e1fe7ea331a09ca9221ebf64c7a798426
Adalid Villanueva <avh465@inlumine.ual.es>
2019-06-09 04:31:35 +0200

    prueba

--- admin/list_users.php
+++ admin/list_users.php
@@ -22,19 +22,14 @@
 
     <table style="width:100%">
           <tr>
-              <th>id</th>
-              <th>username</th>
-              <th>email</th>
-              <th>user_type</th>
-              <th>password</th>
+              <th>ID</th>
+              <th>Username</th>
+              <th>E-mail</th>
+              <th>Role</th>
+              <th>Password</th>
           </tr>
         <?php
-        $conn = mysqli_connect('127.0.0.1:49452', 'azure', '6#vWHD_$', 'multi_login');
-        if($conn -> connect_error){
-          die("Connection failed: ". $conn-> connect_error);
-        }
-        $sql  = "SELECT * FROM users";
-        $result = $conn->$query($sql);
+        $result = getusers();
         if($result -> num_rows > 0){
           while($row = mysql_fetch_array($result)){
               echo "<tr><td>".$row["id"]."</td><td>".$row["username"]."</td><td>".$row["email"].
