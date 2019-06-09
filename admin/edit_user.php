c729e90fa99fd221a4952c365901b2c48f1124aa
Adalid Villanueva <avh465@inlumine.ual.es>
2019-06-09 17:45:50 +0200

    test

--- admin/edit_user.php
+++ admin/edit_user.php
@@ -1,4 +1,20 @@
 <?php include('../functions.php') ?>
+
+<?php
+if (isset($_GET['edit'])) {
+		$id = $_GET['edit'];
+		$update = true;
+		$record = mysqli_query($db, "SELECT * FROM info WHERE id=$id");
+
+		if (count($record) == 1 ) {
+			$n = mysqli_fetch_array($record);
+			$username = $n['username'];
+			$email = $n['email'];
+			$user_type = $n['user_type'];
+		}
+	}
+	?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -20,20 +36,7 @@
 
 	<form method="post" action="edit_user.php">
 
-		<?php echo display_error();
-		if (isset($_GET['edit'])) {
-				$id = $_GET['edit'];
-				$update = true;
-				$record = mysqli_query($db, "SELECT * FROM info WHERE id=$id");
-
-				if (count($record) == 1 ) {
-					$n = mysqli_fetch_array($record);
-					$username = $n['username'];
-					$email = $n['email'];
-		      $user_type = $n['user_type'];
-				}
-			}
-		?>
+		<?php echo display_error(); ?>
 
 		<div class="input-group">
 			<input type="hidden" name="id" value="<?php echo $id; ?>">
