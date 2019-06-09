d726f993227daf59f2a973bab65ff164b743a2ab
Adalid Villanueva <avh465@inlumine.ual.es>
2019-06-09 13:13:49 +0200

    prueba

--- admin/list_users.php
+++ admin/list_users.php
@@ -38,8 +38,7 @@
                 .$row['username']."</td><td>"
                 .$row['email']."</td><td>"
                 .$row['user_type']."</td><td>"
-                .$row['password']."</td><td>
-                </td></tr>";
+                .$row['password']."</td><td><a href="create_user.php"><div style="height:100%;width:100%">create_user</div></a></td></tr>";
             }
           ?>
           </tbody>
