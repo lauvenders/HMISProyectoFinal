d51cf74fe74d42d63785c1a0b307c5cd1231a1b9
Adalid Villanueva <avh465@inlumine.ual.es>
2019-06-09 04:09:27 +0200

    prueba

--- admin/list_users.php
+++ admin/list_users.php
@@ -29,7 +29,7 @@
               <th>password</th>
           </tr>
         <?php
-        $conn = mysqli_connect('127.0.0.1:49452', 'azure', '6#vWHD_$', 'multi_login');
+        $conn = new PDO('127.0.0.1:49452', 'azure', '6#vWHD_$', 'multi_login');
         if($conn -> connect_error){
           die("Connection failed: ". $conn-> connect_error);
         }
